1a4124983ddf71c0d33c809474bb630a5bb34aec
AliAkbar001 <786aliakbar.aa@gmail.com>
2022-02-28 19:28:56 +0500

    Dynamic-Items

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ app.listen(3000)
 
 app.get('/',(req,res)=>{
     //res.sendFile("./views/index.html", {root:__dirname})
-    res.render('index')
+    const items = [
+        {name: 'Mobile Phone',price: 1000 },
+        {name: 'Book',price: 550 },
+        {name: 'Computer',price: 5000 }
+    ]
+    res.render('index',{items})
 })
 
 app.get('/add-item',(req,res)=>{
