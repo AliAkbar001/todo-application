5c0327169413f61815648a01df2b87b61476db0f
AliAkbar001 <786aliakbar.aa@gmail.com>
2022-03-01 14:20:29 +0500

    Save-Item

--- index.js
+++ index.js
@@ -8,26 +8,28 @@ mongoose.connect(CONNECTION_URL).then(()=>
     console.log('Connected'),
     app.listen(3000)
 ).catch(err=>console.log(err))
+
 app.set('view engine', 'ejs')
-
-
+app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
     //res.sendFile("./views/index.html", {root:__dirname})
-    const items = [
-        {name: 'Mobile Phone',price: 1000 },
-        {name: 'Book',price: 550 },
-        {name: 'Computer',price: 5000 }
-    ]
-    res.render('index',{items})
+    res.redirect('/get-items')
 })
 
 app.get('/get-items',(req,res)=>{
-   Item.find().then(result => res.send(result)).catch(err=>console.log(err))
+   Item.find().then(result => res.render('index',{items:result})).catch(err=>console.log(err))
 })
-
+app.get('/get-item',(req,res)=>{
+    Item.find().then(result => res.send(result)).catch(err=>console.log(err))
+})
 app.get('/add-item',(req,res)=>{
     //res.sendFile("./views/add-item.html", {root:__dirname})
     res.render('add-item')
+})
+app.post('/item',(req,res)=>{
+    console.log(req.body)
+    const item = Item(req.body)
+    item.save().then(() => res.redirect('/')).catch(err=>console.log(err))
 })
 
 app.use((req,res)=>{
